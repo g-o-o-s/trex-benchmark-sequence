48570955204e0b5aa891fefdf372f84d1b9a74e6
Edwin Pers <pers.edwin@gmail.com>
2022-08-10 02:51:56 -0400

    add trex api

--- pyastf/astf.py
+++ pyastf/astf.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python2
 
 from trex.astf.api import *
 
@@ -14,7 +14,7 @@ class JsonOutput:
     self.timestamp = timestamp
     self.stats = stats
 
-def astf_test(server, mult, duration, profile_path, outputPath):
+def astf_test(server, mult, duration, profile_path, sleeptime):
 
   # load ASTF profile
   if not profile_path:
@@ -35,32 +35,24 @@ def astf_test(server, mult, duration, profile_path, outputPath):
 
   c.start(mult = mult, duration = duration)
 
-  statsArray = []
-
   while c.is_traffic_active():
       stats = c.get_stats()
 
       timestamp = time.mktime(datetime.datetime.now().timetuple()) * 1000
-
-      statsArray.append(JsonOutput(timestamp, stats))
-
       readableTime = datetime.datetime.fromtimestamp(timestamp / 1000)
       readableTime = readableTime.strftime('%H:%M:%S')
+
       tx_bps = sizeof_fmt(stats['global']['tx_bps'])
       rx_bps = sizeof_fmt(stats['global']['rx_bps'])
       tx_pps = round(stats['global']['tx_pps'] / 1000, 2)
       rx_pps = round(stats['global']['rx_pps'] / 1000, 2)
-
       rx_drop_bps = sizeof_fmt(stats['global']['rx_drop_bps'])
 
-      toPrint = "[%s] bps: [%s / %s] pps: [%dK / %dK] drop: [ %s ]\n" % (readableTime, tx_bps, rx_bps, tx_pps, rx_pps, rx_drop_bps)
+      humanReadable = "[%s] bps: [%s / %s] pps: [%dK / %dK] drop: [ %s ]\n" % (readableTime, tx_bps, rx_bps, tx_pps, rx_pps, rx_drop_bps)
 
-      sys.stderr.write(toPrint)
-
-      time.sleep(0.5)
-
-  outputFile = open(outputPath, "w")
-  outputFile.write(json.dumps(statsArray, default=vars))
+      sys.stderr.write(humanReadable)
+      print(json.dumps(JsonOutput(timestamp, stats), default=vars))
+      time.sleep(sleeptime)
 
   if c.get_warnings():
       sys.stderr.write('\n\n*** test had warnings ****\n\n')
@@ -75,7 +67,7 @@ def astf_test(server, mult, duration, profile_path, outputPath):
 
 def parse_args():
     parser = argparse.ArgumentParser(description = 'TRex ASTF Runner')
-    parser.add_argument('-s',
+    parser.add_argument('-r',
                         dest = 'server',
                         help='remote TRex address',
                         default='127.0.0.1',
@@ -94,10 +86,11 @@ def parse_args():
                         dest = 'duration',
                         help='duration of traffic, default is 10 sec',
                         type = float)
-    parser.add_argument('-o',
-                        dest = 'outputPath',
-                        help='JSON output file. Required',
-                        type = str)
+    parser.add_argument('-s',
+                        default = 0.5,
+                        dest = 'sleeptime',
+                        help='sleep between samples, default is 0.5 sec',
+                        type = float)
 
     return parser.parse_args()
 
@@ -112,4 +105,4 @@ def sizeof_fmt(num, use_kibibyte=True):
     return "%3.1f %s" % (num, x)
 
 args = parse_args()
-astf_test(args.server, args.mult, args.duration, args.file, args.outputPath)
+astf_test(args.server, args.mult, args.duration, args.file, args.sleeptime)
