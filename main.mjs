dec530dbe941f13881c5354c73af3f30834efaa5
Edwin Pers <pers.edwin@gmail.com>
2022-08-10 10:36:31 -0400

    change config profile loading, change output path generation, move example outputs

--- main.mjs
+++ main.mjs
@@ -7,38 +7,43 @@ import 'chartjs-adapter-date-fns';
 import { default as clog } from 'ee-log';
 import { default as Hjson } from 'hjson';
 import { default as child_process } from 'child_process';
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
 
 console.log(`--- Logs begin at ${new Date().toUTCString()} ---`)
 
 // path relative to repo root
 const astfpyPath = 'pyastf/astf.py'
 
+// node main.mjs foo.hjson
 const testProfile = process.argv[2];
 
-const profiles = Hjson.parse(readFileSync(`profiles/${testProfile}`).toString());
+const config = Hjson.parse(readFileSync(`profiles/${testProfile}`).toString());
 
-for (let i = 0; i < profiles.length; i++) {
-  const prof = profiles[i];
+for (let i = 0; i < config.testProfiles.length; i++) {
+  // whoami
+  const prof = config.testProfiles[i];
   clog.info('Spawning profile', prof.name);
   clog.debug('Profile config', prof);
 
+  // setup array for json output file
   const outputJson = [];
 
+  // note the use of spawnSync here - this could be improved with like, locking and a worker pool and stuff
   const child = child_process.spawnSync(
     astfpyPath,
     [
-      '-r', `${prof.server}`,
+      '-r', `${config.server}`,
       '-m', `${prof.mult}`,
       '-d', `${prof.duration}`,
-      '-f', `trex-core/scripts/${prof.file}`,
-      '-s', `${prof.sleep}`
+      '-f', `${prof.file}`,
+      '-s', `${config.sleep}`
     ],
     {
       stdio: ['ignore', 'pipe', 'inherit'],
     }
   );
 
+  // setup datasets for graph
   const data = {
     datasets: [{
       label: 'tx_bps',
@@ -66,13 +71,17 @@ for (let i = 0; i < profiles.length; i++) {
     }]
   };
 
+  // process stdout
   const stdout = child.stdout.toString().split('\n');
   for (let j = 0; j < stdout.length; j++) {
     var input = stdout[j].toString();
+
+    // yes this happens even though the python callee never outputs a blank line...
     if (input != '') {
       input = JSON.parse(input);
       outputJson.push(input);
 
+      // this is all for the human readable
       const timestamp = new Date(input.timestamp);
       const hours = timestamp.getHours();
       const minutes = "0" + timestamp.getMinutes();
@@ -82,7 +91,8 @@ for (let i = 0; i < profiles.length; i++) {
       const tx_bps = input.stats.global.tx_bps / 1000000;
       const rx_bps = input.stats.global.rx_bps / 1000000;
       const rx_drop_bps = input.stats.global.rx_drop_bps / 1000000;
-    
+
+      // and here we push data for the graph
       data.datasets[0].data.push({
         x: timestamp,
         y: tx_bps
@@ -98,9 +108,9 @@ for (let i = 0; i < profiles.length; i++) {
     }
   }
 
+  // setup graph title and config
   const graphTitle = `m:${prof.mult} d:${prof.duration} f:${prof.file}`;
-
-  const config = {
+  const graphConfig = {
     type: 'line',
     data: data,
     options: {
@@ -178,17 +188,34 @@ for (let i = 0; i < profiles.length; i++) {
       }
     },
   };
+
+  // do the graph
   const canvas = Canvas.createCanvas(2400,800)
-  const ctx = canvas.getContext('2d')
-    const myChart = new Chartjs.Chart(
-    ctx,
-    config
+  const canvasContext = canvas.getContext('2d')
+  const graph = new Chartjs.Chart(
+    canvasContext,
+    graphConfig
   );
 
-  const b64Data = myChart.toBase64Image();
-  const b64Image = b64Data.replace(/^data:image\/\w+;base64,/, '');
+  // convert graph to base64 and set it up for being stuffed into a file
+  const graphB64Data = graph.toBase64Image();
+  const graphB64Image = graphB64Data.replace(/^data:image\/\w+;base64,/, '');
 
+  // build our dirs
   const timestamp = Date.now();
-  writeFileSync(`output/graph/${prof.name}-mult_${prof.mult}-dur_${prof.duration}-sleep_${prof.sleep}-${timestamp}.png`, b64Image, {encoding: 'base64'});
-  writeFileSync(`output/json/${prof.name}-mult_${prof.mult}-dur_${prof.duration}-sleep_${prof.sleep}-${timestamp}.json`, JSON.stringify(outputJson));
+  const filePath = `output/${config.dutName}/${config.runName}/${prof.name}`
+  const fileName = `${prof.name}-mult_${prof.mult}-dur_${prof.duration}-${timestamp}`
+
+  // create the output dir if required
+  if (!existsSync(filePath)){
+    mkdirSync(filePath, { recursive: true });
+  }
+
+  // and finally output our files
+  writeFileSync(`${filePath}/${fileName}.png`, graphB64Image, {encoding: 'base64'});
+  writeFileSync(`${filePath}/${fileName}.json`, JSON.stringify(outputJson));
+  
+  console.log(`Wrote output to ${filePath}/`)
+
+  // GOTO 10
 }
