03eb9f9db1ffc65e7aa1694de20141299695b934
Edwin Pers <pers.edwin@gmail.com>
2022-08-11 12:59:19 -0400

    split graphing and summary stats into their own files

--- main.mjs
+++ main.mjs
@@ -10,6 +10,10 @@ import { default as child_process } from 'child_process';
 import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
 import { default as prettyms } from 'pretty-ms';
 
+import { default as bpsGraph } from './lib/graphs/bps.mjs';
+import { default as ppsGraph } from './lib/graphs/pps.mjs';
+import { default as flowsGraph } from './lib/graphs/flows.mjs';
+import { default as summaryStats } from './lib/humanreadable/summaryStats.mjs';
 
 const initTime = new Date();
 
@@ -30,15 +34,10 @@ if (!existsSync(filePath)){
 }
 writeFileSync(`${filePath}/profile.json`, JSON.stringify(config, null, 2));
 
-const color1 = '216, 17, 89';
-const color2 = '143, 45, 86';
-const color3 = '33, 131, 128';
-const color4 = '251, 177, 60';
-const color5 = '115, 210, 222';
-
 for (let i = 0; i < config.testProfiles.length; i++) {
   // # 10
 
+  // sleep for 5 seconds to allow previous runs to fully exit
   await new Promise(r => setTimeout(r, 5000));
 
   // whoami
@@ -46,7 +45,6 @@ for (let i = 0; i < config.testProfiles.length; i++) {
 
   // Is the profile enabled?
   if (prof.enabled === true) {
-
     const startTime = new Date();
     console.log(`--- Starting run ${prof.name} at ${startTime.toUTCString()} ---`);
 
@@ -54,7 +52,7 @@ for (let i = 0; i < config.testProfiles.length; i++) {
     const outputJson = [];
 
     // object of arrays for final human readable stats
-    const summaryStats = {
+    const summaryStatsData = {
       tx_bps: [],
       rx_bps: [],
       tx_pps: [],
@@ -64,76 +62,20 @@ for (let i = 0; i < config.testProfiles.length; i++) {
       tx_cps: [],
     };
 
-    // setup datasets for graph
     const bpsData = {
-      datasets: [
-        {
-          label: 'tx_bps',
-          borderColor: `rgb(${color1})`,
-          backgroundColor: `rgba(${color1}, 0.1)`,
-          fill: 'origin',
-          yAxisID: 'yRight',
-          data: [],
-        },
-        {
-          label: 'rx_bps',
-          borderColor: `rgb(${color3})`,
-          backgroundColor: `rgba(${color3}, 0.1)`,
-          fill: 'origin',
-          yAxisID: 'yRight',
-          data: [],
-        },
-        {
-          label: 'rx_drop_bps',
-          borderColor: `rgb(${color4})`,
-          backgroundColor: `rgba(${color4}, 0.1)`,
-          fill: 'origin',
-          yAxisID: 'yRight',
-          data: [],
-        }
-      ]
+      tx_bps: [],
+      rx_bps: [],
+      rx_drop_bps: [],
     };
 
     const ppsData = {
-      datasets: [
-        {
-          label: 'tx_pps',
-          borderColor: `rgb(${color1})`,
-          backgroundColor: `rgba(${color1}, 0.1)`,
-          fill: 'origin',
-          yAxisID: 'yRight',
-          data: [],
-        },
-        {
-          label: 'rx_pps',
-          borderColor: `rgb(${color3})`,
-          backgroundColor: `rgba(${color3}, 0.1)`,
-          fill: 'origin',
-          yAxisID: 'yRight',
-          data: [],
-        },
-      ]
+      tx_pps: [],
+      rx_pps: [],
     };
 
     const flowsData = {
-      datasets: [
-        {
-          label: 'tx_cps',
-          borderColor: `rgb(${color1})`,
-          backgroundColor: `rgba(${color1}, 0.1)`,
-          fill: 'origin',
-          yAxisID: 'yLeft',
-          data: [],
-        },
-        {
-          label: 'active_flows',
-          borderColor: `rgb(${color3})`,
-          backgroundColor: `rgba(${color3}, 0.1)`,
-          fill: 'origin',
-          yAxisID: 'yRight',
-          data: [],
-        }
-      ]
+      tx_cps: [],
+      active_flows: [],
     };
 
     // note the use of spawnSync here - this could be improved with like, locking and a worker pool and stuff
@@ -173,51 +115,44 @@ for (let i = 0; i < config.testProfiles.length; i++) {
           // this is all for the human readable
           const timestamp = new Date(input.timestamp);
 
-          const tx_bps = input.stats.global.tx_bps / 1000000;
-          const rx_bps = input.stats.global.rx_bps / 1000000;
-          const tx_pps = input.stats.global.tx_pps;
-          const rx_pps = input.stats.global.rx_pps;
-          const rx_drop_bps = input.stats.global.rx_drop_bps / 1000000;
-          const active_flows = input.stats.global.active_flows;
-          const tx_cps = input.stats.global.tx_cps;
-
           // push data for our summary stats at the end
-          summaryStats.tx_bps.push(tx_bps);
-          summaryStats.rx_bps.push(rx_bps);
-          summaryStats.tx_pps.push(tx_pps);
-          summaryStats.rx_pps.push(rx_pps);
-          summaryStats.rx_drop_bps.push(rx_drop_bps);
-          summaryStats.active_flows.push(active_flows);
-          summaryStats.tx_cps.push(tx_cps);
+          summaryStatsData.tx_bps.push(input.stats.global.tx_bps);
+          summaryStatsData.rx_bps.push(input.stats.global.rx_bps);
+          summaryStatsData.tx_pps.push(input.stats.global.tx_pps);
+          summaryStatsData.rx_pps.push(input.stats.global.rx_pps);
+          summaryStatsData.rx_drop_bps.push(input.stats.global.rx_drop_bps);
+          summaryStatsData.active_flows.push(input.stats.global.active_flows);
+          summaryStatsData.tx_cps.push(input.stats.global.tx_cps);
 
-          // and here we push data for the graph
-          bpsData.datasets[0].data.push({
+          bpsData.tx_bps.push({
             x: timestamp,
             y: tx_bps
           });
-          bpsData.datasets[1].data.push({
+
+          bpsData.rx_bps.push({
             x: timestamp,
             y: rx_bps
           });
-          bpsData.datasets[2].data.push({
+
+          bpsData.rx_drop_bps.push({
             x: timestamp,
             y: rx_drop_bps
           });
 
-          ppsData.datasets[0].data.push({
+          ppsData.tx_pps.push({
             x: timestamp,
             y: tx_pps
           });
-          ppsData.datasets[1].data.push({
+          ppsData.rx_pps.push({
             x: timestamp,
             y: rx_pps
           });
 
-          flowsData.datasets[0].data.push({
+          flowsData.tx_cps.push({
             x: timestamp,
             y: tx_cps
           });
-          flowsData.datasets[1].data.push({
+          flowsData.active_flows.push({
             x: timestamp,
             y: active_flows
           });
@@ -229,384 +164,15 @@ for (let i = 0; i < config.testProfiles.length; i++) {
       }
     }
 
+    // Print summary stats
+    summaryStats.printSummaryStats(summaryStats);
+
     // setup graph title and config
     const graphTitle = `dut:${config.dutName} run:${config.runName} name:${prof.name} mult:${prof.mult} dur:${config.duration} prof:${prof.file}`;
 
-    const bpsGraphConfig = {
-      type: 'line',
-      data: bpsData,
-      options: {
-        plugins: {
-          title: {
-            display: true,
-            text: graphTitle,
-            font: {
-              family: "monospace",
-              size: 30,
-            }
-          },
-          legend: {
-            position: 'top',
-            align: 'left',
-            labels: {
-              boxWidth: 18,
-              boxHeight: 18,
-              font: {
-                family: "monospace",
-                size: 28,
-              }
-            }
-          }
-        },
-        elements: {
-          point: {
-            radius: 0,
-          },
-        },
-        scales: {
-          x: {
-            ticks: {
-              font: {
-                family: "monospace",
-                size: 20,
-              },
-            },
-            type: 'timeseries',
-            time: {
-              displayFormats: {
-                millisecond: 'hh:mm:ss'
-              },
-              unit: 'millisecond',
-              stepSize: 4000,
-            },
-          },
-          yRight: {
-            id: 'yRight',
-            position: 'right',
-            ticks: {
-              font: {
-                family: "monospace",
-                size: 24,
-              }
-            },
-            type: 'linear',
-            title: {
-              text: 'Mbps',
-              display: true,
-              font: {
-                family: "monospace",
-                size: 30,
-              }
-            },
-            grid: {
-              drawOnChartArea: false, // only want the grid lines for one axis to show up
-            },
-          }
-        }
-      },
-      layout: {
-        padding: {
-          top: 20,
-          right: 20,
-          left: 20,
-          bottom: 20
-        }
-      },
-    };
-
-    const ppsGraphConfig = {
-      type: 'line',
-      data: ppsData,
-      options: {
-        plugins: {
-          title: {
-            display: true,
-            text: graphTitle,
-            font: {
-              family: "monospace",
-              size: 30,
-            }
-          },
-          legend: {
-            position: 'top',
-            align: 'left',
-            labels: {
-              boxWidth: 18,
-              boxHeight: 18,
-              font: {
-                family: "monospace",
-                size: 28,
-              }
-            }
-          }
-        },
-        elements: {
-          point: {
-            radius: 0,
-          },
-        },
-        scales: {
-          x: {
-            ticks: {
-              font: {
-                family: "monospace",
-                size: 20,
-              },
-            },
-            type: 'timeseries',
-            time: {
-              displayFormats: {
-                millisecond: 'hh:mm:ss'
-              },
-              unit: 'millisecond',
-              stepSize: 4000,
-            },
-          },
-          yRight: {
-            id: 'yRight',
-            position: 'right',
-            ticks: {
-              font: {
-                family: "monospace",
-                size: 24,
-              }
-            },
-            type: 'linear',
-            title: {
-              text: 'Packets / Second',
-              display: true,
-              font: {
-                family: "monospace",
-                size: 30,
-              }
-            }
-          }
-        }
-      },
-      layout: {
-        padding: {
-          top: 20,
-          right: 20,
-          left: 20,
-          bottom: 20
-        }
-      },
-    };
-
-    const flowsGraphConfig = {
-      type: 'line',
-      data: flowsData,
-      options: {
-        plugins: {
-          title: {
-            display: true,
-            text: graphTitle,
-            font: {
-              family: "monospace",
-              size: 30,
-            }
-          },
-          legend: {
-            position: 'top',
-            align: 'left',
-            labels: {
-              boxWidth: 18,
-              boxHeight: 18,
-              font: {
-                family: "monospace",
-                size: 28,
-              }
-            }
-          }
-        },
-        elements: {
-          point: {
-            radius: 0,
-          },
-        },
-        scales: {
-          x: {
-            ticks: {
-              font: {
-                family: "monospace",
-                size: 20,
-              },
-            },
-            type: 'timeseries',
-            time: {
-              displayFormats: {
-                millisecond: 'hh:mm:ss'
-              },
-              unit: 'millisecond',
-              stepSize: 4000,
-            },
-          },
-          yLeft: {
-            id: 'yLeft',
-            position: 'left',
-            ticks: {
-              font: {
-                family: "monospace",
-                size: 24,
-              }
-            },
-            type: 'linear',
-            title: {
-              text: 'Connections / Second',
-              display: true,
-              font: {
-                family: "monospace",
-                size: 30,
-              }
-            }
-          },
-          yRight: {
-            id: 'yRight',
-            position: 'right',
-            ticks: {
-              font: {
-                family: "monospace",
-                size: 24,
-              }
-            },
-            type: 'linear',
-            title: {
-              text: 'Active Flows',
-              display: true,
-              font: {
-                family: "monospace",
-                size: 30,
-              }
-            }
-          }
-        }
-      },
-      layout: {
-        padding: {
-          top: 20,
-          right: 20,
-          left: 20,
-          bottom: 20
-        }
-      },
-    };
-
-    // do the graph
-    const bpsCanvas = Canvas.createCanvas(2400,800);
-    const bpsCanvasContext = bpsCanvas.getContext('2d');
-    const bpsGraph = new Chartjs.Chart(
-      bpsCanvasContext,
-      bpsGraphConfig
-    );
-
-    const ppsCanvas = Canvas.createCanvas(2400,800);
-    const ppsCanvasContext = ppsCanvas.getContext('2d');
-    const ppsGraph = new Chartjs.Chart(
-      ppsCanvasContext,
-      ppsGraphConfig
-    );
-
-    const flowsCanvas = Canvas.createCanvas(2400,800);
-    const flowsCanvasContext = flowsCanvas.getContext('2d');
-    const flowsGraph = new Chartjs.Chart(
-      flowsCanvasContext,
-      flowsGraphConfig
-    );
-
-    // convert graph to base64 and set it up for being stuffed into a file
-    const bpsGraphB64Data = bpsGraph.toBase64Image();
-    const bpsGraphB64Image = bpsGraphB64Data.replace(/^data:image\/\w+;base64,/, '');
-
-    const ppsGraphB64Data = ppsGraph.toBase64Image();
-    const ppsGraphB64Image = ppsGraphB64Data.replace(/^data:image\/\w+;base64,/, '');
-
-    const flowsGraphB64Data = flowsGraph.toBase64Image();
-    const flowsGraphB64Image = flowsGraphB64Data.replace(/^data:image\/\w+;base64,/, '');
-
-    // some basic analysis
-    summaryStats.tx_bps = filterOutliers(summaryStats.tx_bps);
-    summaryStats.rx_bps = filterOutliers(summaryStats.rx_bps);
-    summaryStats.tx_pps = filterOutliers(summaryStats.tx_pps);
-    summaryStats.rx_pps = filterOutliers(summaryStats.rx_pps);
-    summaryStats.rx_drop_bps = filterOutliers(summaryStats.rx_drop_bps);
-    summaryStats.active_flows = filterOutliers(summaryStats.active_flows);
-    summaryStats.tx_cps = filterOutliers(summaryStats.tx_cps);
-
-    summaryStats.tx_bps_stddev = arr_stddev(summaryStats.tx_bps);
-    summaryStats.rx_bps_stddev = arr_stddev(summaryStats.rx_bps);
-    summaryStats.tx_pps_stddev = arr_stddev(summaryStats.tx_pps);
-    summaryStats.rx_pps_stddev = arr_stddev(summaryStats.rx_pps);
-    summaryStats.rx_drop_bps_stddev = arr_stddev(summaryStats.rx_drop_bps);
-    summaryStats.active_flows_stddev = arr_stddev(summaryStats.active_flows);
-    summaryStats.tx_cps_stddev = arr_stddev(summaryStats.tx_cps);
-
-    summaryStats.tx_bps_mean = arr_mean(summaryStats.tx_bps);
-    summaryStats.rx_bps_mean = arr_mean(summaryStats.rx_bps);
-    summaryStats.tx_pps_mean = arr_mean(summaryStats.tx_pps);
-    summaryStats.rx_pps_mean = arr_mean(summaryStats.rx_pps);
-    summaryStats.rx_drop_bps_mean = arr_mean(summaryStats.rx_drop_bps);
-    summaryStats.active_flows_mean = arr_mean(summaryStats.active_flows);
-    summaryStats.tx_cps_mean = arr_mean(summaryStats.tx_cps);
-
-    summaryStats.tx_bps_max = Math.max(...summaryStats.tx_bps);
-    summaryStats.rx_bps_max = Math.max(...summaryStats.rx_bps);
-    summaryStats.tx_pps_max = Math.max(...summaryStats.tx_pps);
-    summaryStats.rx_pps_max = Math.max(...summaryStats.rx_pps);
-    summaryStats.rx_drop_bps_max = Math.max(...summaryStats.rx_drop_bps);
-    summaryStats.active_flows_max = Math.max(...summaryStats.active_flows);
-    summaryStats.tx_cps_max = Math.max(...summaryStats.tx_cps);
-
-    summaryStats.tx_bps_min = Math.min(...summaryStats.tx_bps);
-    summaryStats.rx_bps_min = Math.min(...summaryStats.rx_bps);
-    summaryStats.tx_pps_min = Math.min(...summaryStats.tx_pps);
-    summaryStats.rx_pps_min = Math.min(...summaryStats.rx_pps);
-    summaryStats.rx_drop_bps_min = Math.min(...summaryStats.rx_drop_bps);
-    summaryStats.active_flows_min = Math.min(...summaryStats.active_flows);
-    summaryStats.tx_cps_min = Math.min(...summaryStats.tx_cps);
-
-    // i love formatting text for output, don't you?
-    console.log('');
-    console.log('All datasets have had outliers trimmed for this summary display.');
-
-    console.log('');
-    console.log('averages:');
-    console.log(`tx_bps: ${summaryStats.tx_bps_mean}`);
-    console.log(`rx_bps: ${summaryStats.rx_bps_mean}`);
-    console.log(`tx_pps: ${summaryStats.tx_pps_mean}`);
-    console.log(`rx_pps: ${summaryStats.rx_pps_mean}`);
-    console.log(`rx_drop_bps: ${summaryStats.rx_drop_bps_mean}`);
-    console.log(`active_flows: ${summaryStats.active_flows_mean}`);
-    console.log(`tx_cps: ${summaryStats.tx_cps_mean}`);
-
-    console.log('');
-    console.log('stddevs:');
-    console.log(`tx_bps: ${summaryStats.tx_bps_stddev}`);
-    console.log(`rx_bps: ${summaryStats.rx_bps_stddev}`);
-    console.log(`tx_pps: ${summaryStats.tx_pps_stddev}`);
-    console.log(`rx_pps: ${summaryStats.rx_pps_stddev}`);
-    console.log(`rx_drop_bps: ${summaryStats.rx_drop_bps_stddev}`);
-    console.log(`active_flows: ${summaryStats.active_flows_stddev}`);
-    console.log(`tx_cps: ${summaryStats.tx_cps_stddev}`);
-
-    console.log('');
-    console.log('max:');
-    console.log(`tx_bps: ${summaryStats.tx_bps_max}`);
-    console.log(`rx_bps: ${summaryStats.rx_bps_max}`);
-    console.log(`tx_pps: ${summaryStats.tx_pps_max}`);
-    console.log(`rx_pps: ${summaryStats.rx_pps_max}`);
-    console.log(`rx_drop_bps: ${summaryStats.rx_drop_bps_max}`);
-    console.log(`active_flows: ${summaryStats.active_flows_max}`);
-    console.log(`tx_cps: ${summaryStats.tx_cps_max}`);
-
-    console.log('');
-    console.log('min:');
-    console.log(`tx_bps: ${summaryStats.tx_bps_min}`);
-    console.log(`rx_bps: ${summaryStats.rx_bps_min}`);
-    console.log(`tx_pps: ${summaryStats.tx_pps_min}`);
-    console.log(`rx_pps: ${summaryStats.rx_pps_min}`);
-    console.log(`rx_drop_bps: ${summaryStats.rx_drop_bps_min}`);
-    console.log(`active_flows: ${summaryStats.active_flows_min}`);
-    console.log(`tx_cps: ${summaryStats.tx_cps_min}`);
+    const bpsGraphB64Image = bpsGraph.drawGraph(bpsData, graphTitle, config, prof);
+    const ppsGraphB64Image = ppsGraph.drawGraph(ppsData, graphTitle, config, prof);
+    const flowsGraphB64Image = flowsGraph.drawGraph(flowsData, graphTitle, config, prof);
 
     console.log('');
 
@@ -624,8 +190,6 @@ for (let i = 0; i < config.testProfiles.length; i++) {
       sign === -1 ? "Over by" : "Under by",
       prettyms(diff)
     );
-
-    console.log('');
 
     // build our dirs
     const filePath = `./output/${config.dutName}/${config.runName}/${initTime.toJSON().replaceAll(':', '-').substring(0, 19)}/${prof.name}/`
