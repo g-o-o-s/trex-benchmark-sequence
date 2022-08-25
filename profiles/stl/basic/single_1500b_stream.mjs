08e9e10a0f61df8492d46599db17244b602757c2
Edwin Pers <pers.edwin@gmail.com>
2022-08-25 00:24:02 -0400

    big updoot, lots of playing around and extending/fixing

--- profiles/stl/basic/single_1500b_stream.mjs
+++ profiles/stl/basic/single_1500b_stream.mjs
@@ -1,6 +1,6 @@
 'use strict';
 
-const stream = [
+const streams = [
   {
     flags: 0,
     action_count: 0,
@@ -12,7 +12,7 @@ const stream = [
     mode: {
       rate: {
         type: 'pps',
-        value: 28,
+        value: 1000,
       },
       type: 'continuous',
     },
@@ -70,146 +70,6 @@ const stream = [
       enabled: false,
     },
   },
-  {
-    flags: 0,
-    action_count: 0,
-    enabled: true,
-    self_start: true,
-    start_paused: false,
-    isg: 0.1,
-    core_id: -1,
-    mode: {
-      rate: {
-        type: 'pps',
-        value: 16,
-      },
-      type: 'continuous',
-    },
-    packet: {
-      binary:
-        'AAAAAQAAAAAAAgAACABFAAJAAAEAAEAROKsQAAABMAAAAQA1ADUCLMo5eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHg=',
-      meta: '',
-    },
-    vm: {
-      instructions: [
-        {
-          type: 'flow_var',
-          name: 'src',
-          size: 4,
-          op: 'inc',
-          next_var: null,
-          split_to_cores: true,
-          init_value: 268435457,
-          min_value: 268435457,
-          max_value: 268435710,
-          step: 1,
-        },
-        {
-          type: 'flow_var',
-          name: 'dst',
-          size: 4,
-          op: 'inc',
-          next_var: null,
-          split_to_cores: true,
-          init_value: 805306369,
-          min_value: 805306369,
-          max_value: 805306622,
-          step: 1,
-        },
-        {
-          type: 'write_flow_var',
-          name: 'src',
-          pkt_offset: 26,
-          add_value: 0,
-          is_big_endian: true,
-        },
-        {
-          type: 'write_flow_var',
-          name: 'dst',
-          pkt_offset: 30,
-          add_value: 0,
-          is_big_endian: true,
-        },
-        {
-          type: 'fix_checksum_ipv4',
-          pkt_offset: 14,
-        },
-      ],
-    },
-    flow_stats: {
-      enabled: false,
-    },
-  },
-  {
-    flags: 0,
-    action_count: 0,
-    enabled: true,
-    self_start: true,
-    start_paused: false,
-    isg: 0.2,
-    core_id: -1,
-    mode: {
-      rate: {
-        type: 'pps',
-        value: 4,
-      },
-      type: 'continuous',
-    },
-    packet: {
-      binary:
-        'AAAAAQAAAAAAAgAACABFAAXcAAEAAEARNQ8QAAABMAAAAQA1ADUFyFmYeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHg=',
-      meta: '',
-    },
-    vm: {
-      instructions: [
-        {
-          type: 'flow_var',
-          name: 'src',
-          size: 4,
-          op: 'inc',
-          next_var: null,
-          split_to_cores: true,
-          init_value: 268435457,
-          min_value: 268435457,
-          max_value: 268435710,
-          step: 1,
-        },
-        {
-          type: 'flow_var',
-          name: 'dst',
-          size: 4,
-          op: 'inc',
-          next_var: null,
-          split_to_cores: true,
-          init_value: 805306369,
-          min_value: 805306369,
-          max_value: 805306622,
-          step: 1,
-        },
-        {
-          type: 'write_flow_var',
-          name: 'src',
-          pkt_offset: 26,
-          add_value: 0,
-          is_big_endian: true,
-        },
-        {
-          type: 'write_flow_var',
-          name: 'dst',
-          pkt_offset: 30,
-          add_value: 0,
-          is_big_endian: true,
-        },
-        {
-          type: 'fix_checksum_ipv4',
-          pkt_offset: 14,
-        },
-      ],
-    },
-    flow_stats: {
-      enabled: false,
-    },
-  },
 ];
 
-export default stream;
+export default streams;
